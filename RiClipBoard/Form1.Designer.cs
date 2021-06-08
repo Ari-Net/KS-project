4bd61551fcf34e3063faef36ed7c5960a1043d7b
Ari-Net <73112729+Ari-Net@users.noreply.github.com>
2021-06-08 15:58:24 +0300

    Project - Romoting clipboard
    Helps to share clipboard between computers faster and simply

---
+++ RiClipBoard/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace RiClipBoard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.computerName = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.allowIncomingCB = new System.Windows.Forms.CheckBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sendClipboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.allowIncomingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // computerName
+            // 
+            this.computerName.Location = new System.Drawing.Point(12, 33);
+            this.computerName.Name = "computerName";
+            this.computerName.Size = new System.Drawing.Size(185, 22);
+            this.computerName.TabIndex = 0;
+            // 
+            // sendButton
+            // 
+            this.sendButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.sendButton.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sendButton.ForeColor = System.Drawing.Color.White;
+            this.sendButton.Location = new System.Drawing.Point(12, 103);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(185, 29);
+            this.sendButton.TabIndex = 1;
+            this.sendButton.Text = "SEND";
+            this.sendButton.UseVisualStyleBackColor = false;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "RiClipBoard";
+            this.notifyIcon1.Visible = true;
+            // 
+            // allowIncomingCB
+            // 
+            this.allowIncomingCB.Checked = true;
+            this.allowIncomingCB.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.allowIncomingCB.ForeColor = System.Drawing.Color.Black;
+            this.allowIncomingCB.Location = new System.Drawing.Point(12, 55);
+            this.allowIncomingCB.Name = "allowIncomingCB";
+            this.allowIncomingCB.Size = new System.Drawing.Size(185, 42);
+            this.allowIncomingCB.TabIndex = 4;
+            this.allowIncomingCB.Text = "Allow others to send clipboard to this computer";
+            this.allowIncomingCB.CheckedChanged += new System.EventHandler(this.allowIncomingCB_CheckedChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sendClipboardToolStripMenuItem,
+            this.allowIncomingToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(159, 98);
+            this.contextMenuStrip1.Text = "RiClipBoard";
+            // 
+            // sendClipboardToolStripMenuItem
+            // 
+            this.sendClipboardToolStripMenuItem.Name = "sendClipboardToolStripMenuItem";
+            this.sendClipboardToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.sendClipboardToolStripMenuItem.Text = "Send Clipboard";
+            this.sendClipboardToolStripMenuItem.Click += new System.EventHandler(this.sendClipboardToolStripMenuItem_Click);
+            // 
+            // allowIncomingToolStripMenuItem
+            // 
+            this.allowIncomingToolStripMenuItem.Checked = true;
+            this.allowIncomingToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.allowIncomingToolStripMenuItem.Name = "allowIncomingToolStripMenuItem";
+            this.allowIncomingToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.allowIncomingToolStripMenuItem.Text = "Allow incoming";
+            this.allowIncomingToolStripMenuItem.Click += new System.EventHandler(this.allowIncomingToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(155, 6);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input destination computer name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(207, 140);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.allowIncomingCB);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.computerName);
+            this.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RiClipBoard";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox computerName;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.CheckBox allowIncomingCB;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sendClipboardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem allowIncomingToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ RiClipBoard/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RiClipBoard
+{
+    public partial class Form1 : Form
+    {
+        private RemoteClipboard m_remoteClipboard;
+        public Form1()
+        {
+            InitializeComponent();
+			TcpChannel channel = new TcpChannel(4820);
+			ChannelServices.RegisterChannel(channel, false);
+			RemotingConfiguration.RegisterWellKnownServiceType(typeof(RemoteClipboard), "RiClipBoard", WellKnownObjectMode.Singleton);
+
+			// set the callback for when an incoming clipboard event comes in
+			RemoteClipboard.SetOnClipReceive(this, new ClipEventHandler(this.AddToClip));
+			notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+		}
+
+		// Activate an instance of the RemoteClipboard class on the remote computer
+		private void InitRemoteObject()
+		{
+			try
+			{
+				string location = "tcp://" + computerName.Text + ":4820/RiClipBoard";
+				m_remoteClipboard = (RemoteClipboard)Activator.GetObject(typeof(RemoteClipboard), location);
+				computerName.Modified = false;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+
+		// Callback for the RemoteClipboard class when an incoming cliboard event comes in.
+		public void AddToClip(ArrayList theData)
+		{
+			if (!allowIncomingCB.Checked)
+				throw new Exception("Remote computer has disabled clipboard sharing");
+
+			try
+			{
+				DataObject dataObj = new DataObject();
+				for (int i = 0; i < theData.Count; i++)
+				{
+					string format = (string)theData[i++];
+					dataObj.SetData(format, theData[i]);
+				}
+
+				Clipboard.SetDataObject(dataObj, true);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
+
+		private void sendButton_Click(object sender, EventArgs e)
+        {
+			SendClipboardToRemote();
+		}
+		// Update the tooltip for the system tray icon to show the name of the computer
+		// that the clipboard will be sent to
+		private void UpdateTooltip()
+		{
+			string tooltip = "RiClipBoard";
+			if (computerName.Text.Length > 0)
+				tooltip += " (" + computerName.Text + ')';
+			notifyIcon1.Text = tooltip;
+		}
+		private void SendClipboardToRemote()
+		{
+			try
+			{
+				if (computerName.Text.Length == 0)
+				{
+					MessageBox.Show(this, "Please specify the name of the computer to send the contents of the clipboard to.",
+						"RiClipBoard", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+					return;
+				}
+
+				if (m_remoteClipboard == null || computerName.Modified)
+				{
+					UpdateTooltip();
+					InitRemoteObject();
+				}
+
+				// The dataObject isn't serializable, so inspect each format on the clipboard and if
+				// serializable, then add it to a collection to be sent.
+				ArrayList dataObjects = new ArrayList();
+				IDataObject clipboardData = Clipboard.GetDataObject();
+				string[] formats = clipboardData.GetFormats();
+				for (int i = 0; i < formats.Length; i++)
+				{
+					object clipboardItem = clipboardData.GetData(formats[i]);
+					if (clipboardItem != null && clipboardItem.GetType().IsSerializable)
+					{
+						Console.WriteLine("sending {0}", formats[i]);
+						// for each item, save the format string followed by the clipboard data
+						dataObjects.Add(formats[i]);
+						dataObjects.Add(clipboardItem);
+					}
+					else
+						Console.WriteLine("ignoring {0}", formats[i]);
+				}
+
+				// Send the clipboard data in the array list if there is any
+				if (dataObjects.Count > 0)
+				{
+					Cursor.Current = Cursors.WaitCursor;
+					m_remoteClipboard.SendClipboard(dataObjects);
+					Cursor.Current = Cursors.Default;
+				}
+				else
+					MessageBox.Show(this, "Nothing on clipboard, or contents not supported", "RiClipBoard");
+			}
+			catch (Exception ex)
+			{
+				string message = String.Format("Unable to send data: {0}", ex.Message);
+				MessageBox.Show(this, message, "RiClipBoard");
+			}
+		}
+
+        private void allowIncomingCB_CheckedChanged(object sender, EventArgs e)
+        {
+			allowIncomingToolStripMenuItem.Checked = allowIncomingCB.Checked;
+		}
+
+        
+		// keep context menu on system tray icon and checkbox on form in sync
+		private void allowIncomingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+			allowIncomingToolStripMenuItem.Checked = !allowIncomingToolStripMenuItem.Checked;
+			allowIncomingCB.Checked = allowIncomingToolStripMenuItem.Checked;
+		}
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+			if (this.WindowState == FormWindowState.Minimized)
+			{
+				this.Show();
+				this.WindowState = FormWindowState.Normal;
+			}
+			this.Focus();
+		}
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+			Close();
+        }
+
+        private void sendClipboardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+			SendClipboardToRemote();
+		}
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+			MessageBox.Show("This program helps to share information from clipboard with devices. " +
+				"To share you need to copy text and enter name of computer you want to share it to. " +
+				"You also need to run the program on both computers. Hope you will enjoy. "+
+				"Created by AriNet", "Hello dear friend!");
+		}
+    }
+
+    /// The RemoteClipboard class gets activated remotely to send clipboard data from one computer to another.
+    /// The delegate is used to notify the form that clipboard data has been received.
+    public delegate void ClipEventHandler(ArrayList clipData);
+	public class RemoteClipboard : MarshalByRefObject
+	{
+		private static ClipEventHandler m_OnClipReceive;
+		private static Form m_receiverForm;
+
+		// set the object and method that gets a callback when a clipboard is sent.
+		public static void SetOnClipReceive(Form receiver, ClipEventHandler theCallback)
+		{
+			m_receiverForm = receiver;
+			m_OnClipReceive = theCallback;
+		}
+
+		// this is the method that will be invoked remotely by the other computer.
+		public void SendClipboard(ArrayList clipData)
+		{
+			// Would be nice to be able to just call Clipboard.SetDataObject here, but this is invoked
+			// in an MTA thread instead of STA, and doing any ole operation such as setData is not
+			// allowed.  Calling invoke will get us back to the form's thread which is an [STAThread].
+			//MessageBox.Show("Receiving Clipboard data");
+			object[] clipObjects = { clipData };
+			m_receiverForm.Invoke(m_OnClipReceive, clipObjects);
+		}
+	}
+}
